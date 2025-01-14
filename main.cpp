08b9c14e8964048ecdd1702fc2ee551b9a78e2f8
Michael Floyd <mfloyd16@ut.utm.edu>
2025-01-14 16:45:56 -0600

    Added sum feature

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
+int sum (int n);
+
 int main() {
+  int number;
   cout << "Hello World!" << endl;
+
+  cout << "Pick a number? ";
+  cin >> number;
+
+  cout << "Your sum is " << sum(number) << endl;
   
   return 0;
 }
+
+int sum (int n) {
+  int sum = n;
+  for (int i = 0; i < n; i++) {
+    sum += i;
+  }
+  return sum;
+
+} 
