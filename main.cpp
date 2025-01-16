8d4f79bf7f69d97e88ee212157cc8a37241f82bf
Michael Floyd <mfloyd16@ut.utm.edu>
2025-01-16 08:55:56 -0600

    Added product method

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int sum (int n);
 
+int product (int n);
+
 int main() {
   int number;
   cout << "Hello World!" << endl;
@@ -13,6 +15,8 @@ int main() {
   cin >> number;
 
   cout << "Your sum is " << sum(number) << endl;
+
+  cout << "Your product is " << product(number) << endl;
   
   return 0;
 }
@@ -25,3 +29,11 @@ int sum (int n) {
   return sum;
 
 } 
+int product (int n) {
+  int product = n;
+  for (int i = 1; i < n; i++) {
+    product = product * i;
+  }
+  return product;
+}
+
